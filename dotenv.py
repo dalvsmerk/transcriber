43923c240266bd7bed27152141abe276e40d5d71
Vladyslav Umerenko <space.vlad7@gmail.com>
2023-03-15 19:29:18 +0200

    Read dotenv as utf8 encoded

--- dotenv.py
+++ dotenv.py
@@ -1,7 +1,7 @@
 def load_dotenv():
     env = dict()
 
-    with open('.env', 'r') as f:
+    with open('.env', mode='r', encoding='utf-8') as f:
         for line in f.readlines():
             pair = line.replace('\n', '').split('=')
             env[pair[0]] = pair[1]
