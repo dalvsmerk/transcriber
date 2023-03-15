d366d60ba72fd38749953fd189e55183309dc5d1
Vladyslav Umerenko <space.vlad7@gmail.com>
2023-03-15 19:52:44 +0200

    Handle and print exception on audio transcription

--- audio_handler.py
+++ audio_handler.py
@@ -12,12 +12,15 @@ class AudioFileEventHandler(PatternMatchingEventHandler):
 
     def on_created(self, event):
         if not event.is_directory:
-            result = self.model.transcribe(event.src_path)
-            filename, message = self.formatMessage(event.src_path, result)
-            
-            self.logging.log(self.logging.INFO, filename + '\n' + message)
+            try:
+                result = self.model.transcribe(event.src_path)
+                filename, message = self.formatMessage(event.src_path, result)
+                
+                self.logging.log(self.logging.INFO, filename + '\n' + message)
 
-            self.whatsapp.sendGroupMessage(message, title=filename)
+                self.whatsapp.sendGroupMessage(message, title=filename)
+            except Exception as e:
+                print(e)
 
         return super().on_created(event)
     
