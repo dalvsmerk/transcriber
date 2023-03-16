d4e1514f9c686c6d6ec1cd7763994b7847748266
Vladyslav Umerenko <space.vlad7@gmail.com>
2023-03-16 18:53:06 +0200

    Use dtype float32 to run on CPU

--- model.py
+++ model.py
@@ -8,4 +8,4 @@ class Whisper:
 
     def transcribe(self, audio_path):
         return whisper.transcribe(self.model, audio_path, language=self.language, 
-                                  task='transcribe', temperature=0)
+                                  task='transcribe', temperature=0, fp16=False)
