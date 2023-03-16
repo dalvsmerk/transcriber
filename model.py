aea0dce39350f035184c896833703303d86adba9
Vladyslav Umerenko <space.vlad7@gmail.com>
2023-03-16 18:40:16 +0200

    Set whisper model size to small

--- model.py
+++ model.py
@@ -3,8 +3,8 @@ import whisper
 
 class Whisper:
     def __init__(self, language='en'):
-        self.model = whisper.load_model('base')
+        self.model = whisper.load_model('small')
         self.language = language
 
     def transcribe(self, audio_path):
-        return self.model.transcribe(audio_path, language=self.language)
+        return whisper.transcribe(self.model, audio_path, language=self.language, task='transcribe')
