39fb77bfa02fd927c335b5394b24061bfff7722e
Vladyslav Umerenko <space.vlad7@gmail.com>
2023-03-17 17:48:29 +0200

    Read windows encoding

--- dotenv.py
+++ dotenv.py
@@ -1,9 +1,32 @@
+import platform
+
+CP1252 = 'cp1252'
+UTF8 = 'utf-8'
+
+def is_windows():
+    return platform.system() == 'Windows'
+
+
+def get_platform_encoding():
+    return CP1252 if is_windows() else UTF8
+
+
+def try_decode_windows(text):
+    if is_windows():
+        return text.encode(CP1252).decode(UTF8)
+
+    return text
+
+
 def load_dotenv():
     env = dict()
 
-    with open('.env', mode='r', encoding='utf-8') as f:
+    newline = '\r\n' if is_windows() else '\n'
+
+    with open('.env', mode='r', encoding=get_platform_encoding()) as f:
         for line in f.readlines():
-            pair = line.replace('\n', '').split('=')
+            line = try_decode_windows(line)
+            pair = line.replace(newline, '').split('=')
             env[pair[0]] = pair[1]
 
     return env
